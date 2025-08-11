e42c016bba66415277bf825990764e1f94692e04
Ruchi Kumari <136886062+khushi-1907@users.noreply.github.com>
2025-08-11 17:40:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/LCBAD/", // Replace with your repo name
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
   },
   resolve: {
     alias: {
